09df63a9b949c026ed3aa822814c8402315bec7f
marinakhotko <kudzelkamarina@gmail.com>
2022-08-11 16:17:29 +0300

    add some code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+    let m = Math.abs(n);
+    let str = m.toString();
+    let result = "";
+    let i = 0;
   
-}
+    while (i < str.length){
+        result= `${str[i]}${result}`;
+        i +=1;
+    }
+    result = Number(result);
+    return result;
+    }       
